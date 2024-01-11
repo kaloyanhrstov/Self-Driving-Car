b107deb5b447343d4152d5a44ca5694162e3e413
Kaloyan <116978281+kaloyanhrstov@users.noreply.github.com>
2024-01-11 22:07:28 +0200

    Update detecting_lanes.py

--- detecting_lanes.py
+++ detecting_lanes.py
@@ -143,20 +143,24 @@ def process_img(image):
     return processed_img, original_image, m1, m2
 
 
-for i in list(range(4))[::-1]:
-    print(i + 1)
-    time.sleep(1)
-
-last_time = time.time()
-while True:
-    screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))
-    print('Frame took {} seconds'.format(time.time() - last_time))
+def main():
+    for i in list(range(4))[::-1]:
+        print(i + 1)
+        time.sleep(1)
+    
     last_time = time.time()
-    new_screen, original_image, m1, m2 = process_img(screen)
-    cv2.imshow('window', new_screen)
-    cv2.imshow('window2', cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
+    while True:
+        screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))
+        print('Frame took {} seconds'.format(time.time() - last_time))
+        last_time = time.time()
+        new_screen, original_image, m1, m2 = process_img(screen)
+        cv2.imshow('window', new_screen)
+        cv2.imshow('window2', cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
+    
+        # cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
 
-    # cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        break
+
+main()
